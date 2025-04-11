f109f84af312f6aab6823a1487ecab439c8f0ca9
Guillermo <guillef33@gmail.com>
2025-04-11 09:55:27 -0300

    norden api main file

---
+++ norden-api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Norden API Formulario de Cotizacion
+ * Plugin URI:        https://magnetitte.com/
+ * Description:       Plugin para la generacion de formularios de cotizacion con Norden API
+ * Version:           0.0.2
+ * Requires PHP:      7.2
+ * Author:            Guillermo Flores
+ * Author URI:        https://magnetitte.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+?>
