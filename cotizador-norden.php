41d98a239eeaabc67b8b5ee5096755f14015ef85
Martin <martinrotela47@gmail.com>
2025-04-25 14:44:23 +0000

    codigos

--- cotizador-norden.php
+++ cotizador-norden.php
@@ -21,6 +21,7 @@ require_once plugin_dir_path(__FILE__) . 'includes/api-cotizador.php';
 require_once plugin_dir_path(__FILE__) . 'includes/api-provincias.php';
 require_once plugin_dir_path(__FILE__) . 'includes/api-marcas.php';
 require_once plugin_dir_path(__FILE__) . 'includes/api-codigos-postales.php';
+require_once plugin_dir_path(__FILE__) . 'includes/api-modelos.php';
 
 require_once plugin_dir_path(__FILE__) . 'includes/formulario-cotizacion.php';
 
